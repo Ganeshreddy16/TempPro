18a53ff4574f3ee33aa1f5006d15d87ffcea980e
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-23 21:24:14 +0530

    Added: Project Structure

---
+++ src/main/java/org/example/TempPro.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class TempPro
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
